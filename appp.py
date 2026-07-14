263a13dcefa9bd843b5b4aee5e30a1f16beb7b50
vrxshop <testgmeail@gmail.com>
2026-07-14 14:04:58 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -22,7 +22,7 @@ ROLLYPAY_API_KEY = "z39_r_COJdiB7PWeddOYvzT2rx4cjIbS1m4JJcgBTi0"
 ROLLYPAY_CALLBACK_URL = "https://t-bot-18jz.onrender.com/webhook"
 
 # --- КОНФИГУРАЦИЯ БОТА ---
-BOT_TOKEN = "8843954886:AAGdcbtBmEEIIA4-g9B_K85Ez-W20_oWgQw"
+BOT_TOKEN = "8843954886:AAHnRnNU_6ImKapa2EugccBkdv6MonRpV0o"
 PROJECT_NAME = "VIP"
 SUPPORT_CONTACT_RU = "https://t.me/Nastia_sup"
 SUPPORT_CONTACT_EN = "https://t.me/Nastia_sup"
